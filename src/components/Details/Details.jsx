5832b48e09377b4a6a12c633617a1462ae07eae7
Abhishek Harish <itsjustabhii@gmail.com>
2026-08-21 14:40:59 +0530

    added ternary conditional statement for title

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -3,12 +3,12 @@ import {Card, CardHeader, CardContent, Typography} from '@material-ui/core'
 import {Doughnut} from 'react-chartjs-2'
 import useStyles from './styles'
 
-const Details = () => {
+const Details = ({title}) => {
     const classes = useStyles()
 
   return (
-    <Card className={classes.income}>
-        <CardHeader title="Income" />
+    <Card className={title === 'Income' ? classes.income : classes.expense}>
+        <CardHeader title={title} />
         <CardContent>
             <Typography variant="h5">₹ 0</Typography>
             {/* <Doughnut data={data} /> */}
